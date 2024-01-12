38c627509f4b37529847d185d0583f6a609c3f32
william-samwakajr <williamsamwaka05@gmail.com>
2024-01-12 15:34:49 +0200

    added fetch data and its working so far

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-let time;
-let location;
-let weather;
+// weather key = 318351475bbc484fb4a125935241201
+
+async function getWeatherData(location) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=318351475bbc484fb4a125935241201&q=${location}`);
+    const data = await response.json()
+    const city = await data["location"]["name"]
+    console.log(city);
+};
+getWeatherData("lusaka")
