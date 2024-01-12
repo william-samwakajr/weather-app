4a763d300ee9338be610eaabb3ee4791b205bbb2
william-samwakajr <williamsamwaka05@gmail.com>
2024-01-12 15:59:02 +0200

    terminal weather app tested

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 
 async function getWeatherData(location) {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=318351475bbc484fb4a125935241201&q=${location}`);
-    const data = await response.json()
-    const city = await data["location"]["name"]
-    console.log(city);
+
+    const data = await response.json();
+    const city = await data["location"]["name"];
+    const temp = await data["current"]["temp_c"];
+    const condition = await data["current"]["condition"]["text"];
+    const humidity = await data["current"]["humidity"];
+    const cloud = await data["current"]["cloud"];
+
+    console.log(`current location: ${city}`);
+    console.log(`curren temperature: ${temp}`);
+    console.log(`current condition: ${condition}`)
+    console.log(`current humidity: ${humidity}`)
+    console.log(`current cloud cover: ${cloud}`)
 };
-getWeatherData("lusaka")
+getWeatherData("dubai")
