eeeaaf4ae5db296543b83df312815ce0ec1f7c29
william-samwakajr <williamsamwaka05@gmail.com>
2024-01-12 19:53:59 +0200

    made fully functional

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // weather key = 318351475bbc484fb4a125935241201
+let userInput = document.querySelector(".user-input");
+let searchBtn = document.querySelector(".search");
+let cityElem = document.querySelector(".city");
+let tempElem = document.querySelector(".temp");
+let conElem = document.querySelector(".condition");
 
-async function getWeatherData(location) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=318351475bbc484fb4a125935241201&q=${location}`);
+let cloudElem = document.querySelector(".cloud");
+let humidityElem = document.querySelector(".humidity");
+
+
+
+
+async function getWeatherData() {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=318351475bbc484fb4a125935241201&q=${userInput.value}`);
 
     const data = await response.json();
     const city = await data["location"]["name"];
@@ -9,11 +20,19 @@ async function getWeatherData(location) {
     const condition = await data["current"]["condition"]["text"];
     const humidity = await data["current"]["humidity"];
     const cloud = await data["current"]["cloud"];
+    const pic = await data["current"]["condition"]["icon"]
 
-    console.log(`current location: ${city}`);
-    console.log(`curren temperature: ${temp}`);
-    console.log(`current condition: ${condition}`)
-    console.log(`current humidity: ${humidity}`)
-    console.log(`current cloud cover: ${cloud}`)
+    try {
+        cityElem.innerHTML = city;
+        tempElem.innerHTML = `${temp}°C`;
+        conElem.innerHTML = condition;
+        cloudElem.innerHTML = `cloud cover: ${cloud}`
+        document.getElementById("image").src = "//cdn.weatherapi.com/weather/64x64/day/116.png";
+        console.log(pic)
+    } catch (err) {
+        console.error(err)
+
+    }
+    console.log(pic)
 };
-getWeatherData("lusaka")
+searchBtn.addEventListener("click", getWeatherData)
